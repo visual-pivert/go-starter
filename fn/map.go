4324fa188f22008e36ed4bd0b7f4b3bbfdfa244d
visualpivert <visualpivert@gmail.com>
2025-10-23 18:10:09 +0300

    feat: mapReverse function

--- fn/map.go
+++ fn/map.go
@@ -7,3 +7,11 @@ func Map[T any, U any](slice []T, fn func(T) U) []U {
 	}
 	return result
 }
+
+func MapReverse[T any, U any](slice []T, fn func(T) U) []U {
+	result := make([]U, len(slice))
+	for i, value := range slice {
+		result[len(slice)-1-i] = fn(value)
+	}
+	return result
+}
--- fn/map_test.go
+++ fn/map_test.go
@@ -22,3 +22,24 @@ func TestMap(t *testing.T) {
 		})
 	}
 }
+
+func TestMapReverse(t *testing.T) {
+	useCases := []struct {
+		name     string
+		value    []any
+		fn       func(any) any
+		expected []any
+	}{
+		{"multiply by 2 (reversed)", []any{1, 2, 3}, func(value any) any { return value.(int) * 2 }, []any{6, 4, 2}},
+		{"even number (reversed)", []any{1, 2, 3}, func(value any) any { return value.(int)%2 == 0 }, []any{false, true, false}},
+	}
+
+	for _, useCase := range useCases {
+		t.Run(useCase.name, func(t *testing.T) {
+			got := MapReverse(useCase.value, useCase.fn)
+			if !SameSlice(got, useCase.expected) {
+				t.Errorf("Expected %v, got %v", useCase.expected, got)
+			}
+		})
+	}
+}
