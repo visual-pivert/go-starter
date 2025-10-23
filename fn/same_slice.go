4d8df99e822d4fd9e41dd59886a9481cbb80b253
visualpivert <visualpivert@gmail.com>
2025-10-23 13:09:10 +0300

    feat: SameSlice function

---
+++ fn/same_slice.go
@@ -0,0 +1,13 @@
+package fn
+
+func SameSlice[T comparable](slice1 []T, slice2 []T) bool {
+	if len(slice1) != len(slice2) {
+		return false
+	}
+	for i := range slice1 {
+		if slice1[i] != slice2[i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ fn/same_slice_test.go
@@ -0,0 +1,31 @@
+package fn
+
+import "testing"
+
+func TestSameSlice(t *testing.T) {
+	useCases := []struct {
+		name     string
+		value1   []any
+		value2   []any
+		expected bool
+	}{
+		{"same slice of int", []any{1, 2, 3}, []any{1, 2, 3}, true},
+		{"different slice of int", []any{1, 2, 3}, []any{1, 2, 4}, false},
+
+		{"different slice length", []any{1, 2, 3}, []any{1, 2}, false},
+		{"different slice type", []any{1, 2, 3}, []any{"1", "2", "3"}, false},
+		{"nil slice", nil, nil, true},
+
+		{"same slice of string", []any{"a", "b", "c"}, []any{"a", "b", "c"}, true},
+		{"different slice of string", []any{"a", "b", "c"}, []any{"a", "b", "d"}, false},
+	}
+
+	for _, useCase := range useCases {
+		t.Run(useCase.name, func(t *testing.T) {
+			got := SameSlice(useCase.value1, useCase.value2)
+			if got != useCase.expected {
+				t.Errorf("Expected %v, got %v", useCase.expected, got)
+			}
+		})
+	}
+}
