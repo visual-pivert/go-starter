2076245402a18bbdcfbdd738db1d22eba09402d2
visualpivert <visualpivert@gmail.com>
2025-10-23 16:25:29 +0300

    refactor: clean falsy.go, make isZero function reusable

--- fn/falsy.go
+++ fn/falsy.go
@@ -27,34 +27,5 @@ func Falsy(value any) bool {
 	}
 
 	// Check numeric types
-	return isZero(value)
-}
-
-func isZero(value any) bool {
-	switch v := value.(type) {
-	case int:
-		return v == 0
-	case int8:
-		return v == 0
-	case int16:
-		return v == 0
-	case int32:
-		return v == 0
-	case int64:
-		return v == 0
-	case uint:
-		return v == 0
-	case byte:
-		return v == 0
-	case float32:
-		return v == 0
-	case float64:
-		return v == 0
-	case complex64:
-		return v == 0
-	case complex128:
-		return v == 0
-	default:
-		return false
-	}
+	return IsZero(value)
 }
---
+++ fn/is_zero.go
@@ -0,0 +1,30 @@
+package fn
+
+func IsZero(value any) bool {
+	switch v := value.(type) {
+	case int:
+		return v == 0
+	case int8:
+		return v == 0
+	case int16:
+		return v == 0
+	case int32:
+		return v == 0
+	case int64:
+		return v == 0
+	case uint:
+		return v == 0
+	case byte:
+		return v == 0
+	case float32:
+		return v == 0
+	case float64:
+		return v == 0
+	case complex64:
+		return v == 0
+	case complex128:
+		return v == 0
+	default:
+		return false
+	}
+}
