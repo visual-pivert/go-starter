395a6bc467abb41799c5317701c5380605070f85
visualpivert <visualpivert@gmail.com>
2025-10-24 22:09:10 +0300

    test: creating test for Zero function

---
+++ is/zero_test.go
@@ -0,0 +1,23 @@
+package is
+
+import "testing"
+
+func TestZero(t *testing.T) {
+	testCases := []struct {
+		name     string
+		value    any
+		expected bool
+	}{
+		{"zero", 0, true},
+		{"zero float", 0.0, true},
+		{"not zero", 1, false},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			got := Zero(testCase.value)
+			if got != testCase.expected {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
