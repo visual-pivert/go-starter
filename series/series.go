bcbebf88927033a6345825981426225fbb577865
visual-pivert <visualpivert@gmail.com>
2025-11-03 09:44:53 +0300

    feat: Implementing basics series features

---
+++ series/series.go
@@ -0,0 +1,99 @@
+package series
+
+import (
+	visualfn "github.com/visual-pivert/go-starter/fn"
+)
+
+type SeriesType string
+
+const (
+	IntType    SeriesType = "int"
+	FloatType             = "float"
+	StringType            = "string"
+	BoolType              = "bool"
+	TimeType              = "time"
+)
+
+type Series struct {
+	name  string
+	data  []any
+	stype SeriesType
+	len   int
+}
+
+func NewSeries(name string, data []any, t SeriesType) *Series {
+	return &Series{
+		name:  name,
+		data:  data,
+		stype: t,
+		len:   len(data),
+	}
+}
+
+func (s *Series) Copy() *Series {
+	return NewSeries(s.name, s.data, s.stype)
+}
+
+func (s *Series) Len() int {
+	return s.len
+}
+
+func (s *Series) Name() string {
+	return s.name
+}
+
+func (s *Series) Rename(name string) *Series {
+	return NewSeries(name, s.data, s.stype)
+}
+
+func (s *Series) Type() SeriesType {
+	return s.stype
+}
+
+func (s *Series) Get(i int) any {
+	return s.data[i]
+}
+
+func (s *Series) Set(i int, data any) *Series {
+	d := s.Copy()
+	d.data[i] = data
+	return d
+}
+
+func (s *Series) Append(data any) *Series {
+	d := s.Copy()
+	d.data = append(s.data, data)
+	d.len++
+	return d
+}
+
+func (s *Series) AppendSlice(data []any) *Series {
+	d := s.Copy()
+	d.data = append(s.data, data...)
+	d.len += len(data)
+	return d
+}
+
+func (s *Series) FilerToBoolStatement(fn func(any) bool) []bool {
+	return visualfn.FilterToBoolStatement(s.data, fn)
+}
+
+func (s *Series) ApplyWithBoolStatement(boolSlice []bool, fn func(any) any) *Series {
+	d := s.Copy()
+	for i, value := range d.data {
+		if boolSlice[i] {
+			d.data[i] = fn(value)
+		}
+	}
+	return d
+}
+
+func (s *Series) IntersectWithBoolStatement(boolSlice []bool) *Series {
+	d := NewSeries(s.name, []any{}, s.stype)
+	for i, value := range s.data {
+		if boolSlice[i] {
+			d.data = append(d.data, value)
+		}
+	}
+	return d
+}
---
+++ series/series_test.go
@@ -0,0 +1,193 @@
+package series
+
+import (
+	"testing"
+
+	"github.com/visual-pivert/go-starter/fn"
+	"github.com/visual-pivert/go-starter/is"
+)
+
+func TestSeries_Append(t *testing.T) {
+	testCases := []struct {
+		name        string
+		header      string
+		stype       SeriesType
+		value       []any
+		expected    []any
+		expectedLen int
+	}{
+		{"append int value to int series", "number", IntType, []any{1, 2, 3}, []any{1, 2, 3, 4}, 4},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.Append(4)
+			if fn.SameSlice(got.data, testCase.expected) == false {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+			if got.Len() != testCase.expectedLen {
+				tt.Errorf("Expected %v, got %v", testCase.expectedLen, got.Len())
+			}
+		})
+	}
+}
+
+func TestSeries_AppendSeries(t *testing.T) {
+	testCases := []struct {
+		name        string
+		header      string
+		stype       SeriesType
+		value       []any
+		expected    []any
+		expectedLen int
+	}{
+		{"append int slice to int series", "number", IntType, []any{1, 2, 3}, []any{1, 2, 3, 4, 5}, 5},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.AppendSlice([]any{4, 5})
+			if fn.SameSlice(got.data, testCase.expected) == false {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+			if got.Len() != testCase.expectedLen {
+				tt.Errorf("Expected %v, got %v", testCase.expectedLen, got.Len())
+			}
+		})
+	}
+}
+
+func TestSeries_Rename(t *testing.T) {
+	testCases := []struct {
+		name      string
+		header    string
+		stype     SeriesType
+		newHeader string
+		expected  string
+	}{
+		{"rename series", "number", IntType, "new_number", "new_number"},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, []any{1, 2, 3}, testCase.stype)
+			got := series.Rename(testCase.newHeader)
+			if (got.Name() == testCase.expected) == false {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
+
+func TestSeries_Set(t *testing.T) {
+	testCases := []struct {
+		name       string
+		header     string
+		stype      SeriesType
+		value      []any
+		indexToSet int
+		newValue   any
+		expected   []any
+	}{
+		{"set value to series", "number", IntType, []any{1, 2, 3}, 1, 4, []any{1, 4, 3}},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.Set(testCase.indexToSet, testCase.newValue)
+			if fn.SameSlice(got.data, testCase.expected) == false {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
+
+func TestSeries_Get(t *testing.T) {
+	testCases := []struct {
+		name       string
+		header     string
+		stype      SeriesType
+		value      []any
+		indexToGet int
+		expected   any
+	}{
+		{"get value from series", "number", IntType, []any{1, 2, 3}, 1, 2},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.Get(testCase.indexToGet)
+			if got != testCase.expected {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
+
+func TestSeries_FilerToBoolStatement(t *testing.T) {
+	testCases := []struct {
+		name     string
+		header   string
+		stype    SeriesType
+		fn       func(any) bool
+		value    []any
+		expected []bool
+	}{
+		{"filter even number to bool statement", "header", IntType, func(value any) bool { return value.(int)%2 == 0 }, []any{1, 2, 3}, []bool{false, true, false}},
+		{"filter odd number to bool statement", "header", IntType, func(value any) bool { return value.(int)%2 == 1 }, []any{1, 2, 3}, []bool{true, false, true}},
+		{"filter falsy to bool statement", "header", IntType, func(value any) bool { return is.Falsy(value) }, []any{1, 0, "", "a", false}, []bool{false, true, true, false, true}},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.FilerToBoolStatement(testCase.fn)
+			if fn.SameSlice(got, testCase.expected) == false {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
+
+func TestSeries_IntersectWithBoolStatement(t *testing.T) {
+	testCases := []struct {
+		name          string
+		header        string
+		stype         SeriesType
+		value         []any
+		boolStatement []bool
+		expected      []any
+	}{
+		{"intersect with even bool statement", "header", IntType, []any{1, 2, 3}, []bool{false, true, false}, []any{2}},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.IntersectWithBoolStatement(testCase.boolStatement)
+			if fn.SameSlice(got.data, testCase.expected) == false {
+				t.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
+
+func TestSeries_ApplyWithBoolStatement(t *testing.T) {
+	testCases := []struct {
+		name          string
+		header        string
+		stype         SeriesType
+		value         []any
+		fn            func(any) any
+		boolStatement []bool
+		expected      []any
+	}{
+		{"apply multiply by 2 when even", "header", IntType, []any{1, 2, 3}, func(value any) any { return value.(int) * 2 }, []bool{false, true, false}, []any{1, 4, 3}},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(tt *testing.T) {
+			series := NewSeries(testCase.header, testCase.value, testCase.stype)
+			got := series.ApplyWithBoolStatement(testCase.boolStatement, testCase.fn)
+			if fn.SameSlice(got.data, testCase.expected) == false {
+				tt.Errorf("Expected %v, got %v", testCase.expected, got)
+			}
+		})
+	}
+}
