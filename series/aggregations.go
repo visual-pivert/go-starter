c99801845c0114082333277a880cf63313c115e4
visual-pivert <visualpivert@gmail.com>
2025-11-05 18:25:30 +0300

    debug: aggregate but less any param

--- series/aggregations.go
+++ series/aggregations.go
@@ -1,6 +1,6 @@
 package series
 
-func Aggregate(s *Series, initialValue any, fn func(last any, curr any, currIndex int) any) any {
+func Aggregate[T any](s *Series, initialValue T, fn func(last any, curr any, currIndex int) T) T {
 	value := initialValue
 	for i := 0; i < s.Len(); i++ {
 		value = fn(value, s.data[i], i)
