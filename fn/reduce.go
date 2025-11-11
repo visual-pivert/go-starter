031d0c7d098267470369ed2193e9da9db353f975
visual-pivert <visualpivert@gmail.com>
2025-11-11 10:09:28 +0300

    feat: adding index

--- fn/reduce.go
+++ fn/reduce.go
@@ -1,10 +1,10 @@
 package fn
 
-func Reduce[T any](slice []T, initialValue T, fn func(cum T, value T) T) []T {
+func Reduce[T any](slice []T, initialValue T, fn func(cum T, value T, index int) T) []T {
 	out := make([]T, len(slice))
-	out[0] = fn(initialValue, slice[0])
+	out[0] = fn(initialValue, slice[0], 0)
 	for i := 1; i < len(slice); i++ {
-		out[i] = fn(out[i-1], slice[i])
+		out[i] = fn(out[i-1], slice[i], i)
 	}
 	return out
 }
--- fn/reduce_test.go
+++ fn/reduce_test.go
@@ -11,12 +11,13 @@ func TestFn_Reduce(t *testing.T) {
 		name         string
 		initialValue any
 		value        []any
-		fn           func(any, any) any
+		fn           func(any, any, int) any
 		expected     []any
 	}{
-		{"cumsum", 0, []any{1, 2, 3}, func(a, b any) any { return a.(int) + b.(int) }, []any{1, 3, 6}},
-		{"cummultiply", 1, []any{1, 2, 3}, func(a, b any) any { return a.(int) * b.(int) }, []any{1, 2, 6}},
-		{"cumconcat", "", []any{"a", "b", "c"}, func(a, b any) any { return a.(string) + b.(string) }, []any{"a", "ab", "abc"}},
+		{"cumsum", 0, []any{1, 2, 3}, func(a, b any, i int) any { return a.(int) + b.(int) }, []any{1, 3, 6}},
+		{"cummultiply", 1, []any{1, 2, 3}, func(a, b any, i int) any { return a.(int) * b.(int) }, []any{1, 2, 6}},
+		{"cumconcat", "", []any{"a", "b", "c"}, func(a, b any, i int) any { return a.(string) + b.(string) }, []any{"a", "ab", "abc"}},
+		{"cumsum index", 0, []any{2, 3, 4}, func(a, b any, i int) any { return a.(int) + i }, []any{0, 1, 3}},
 	}
 
 	for _, testCase := range testCases {
