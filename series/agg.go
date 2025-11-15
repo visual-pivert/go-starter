06ff8f9078afad28e73e0f9036ab0cce37a9b3d4
visual-pivert <visualpivert@gmail.com>
2025-11-15 11:32:43 +0300

    refactor: bye

--- series/agg.go
+++
@@ -1,9 +0,0 @@
-package series
-
-func Agg[T any](s *Series, initialValue T, fn func(last any, curr any, currIndex int) T) T {
-	value := initialValue
-	for i := 0; i < s.Len(); i++ {
-		value = fn(value, s.data[i], i)
-	}
-	return value
-}
